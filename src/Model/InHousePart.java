bb910adca5059120658ba5427ae380ea41293ff5
Sakae Watanabe <meltdownlv@gmail.com>
2021-04-03 11:38:22 -0700

    Methods and javadoc comments completed for InHouse - OutSourced

--- src/Model/InHousePart.java
+++ src/Model/InHousePart.java
@@ -1,8 +1,45 @@
 package Model;
 
 /**
+ * The InHousePart class holds information about parts produced
+ * internally for use in products.
+ *
  * @author Sakae Watanabe
  */
-public class InHousePart {
+public class InHousePart extends Part {
+  /**The ID of the machine that produced this part.*/
+  private int machineID;
 
+  /**
+   * Constructs a new InHousePart using the specified parameters.
+   *
+   * @param id The ID number for the InHousePart.
+   * @param name The name for the InHousePart.
+   * @param price The price for the InHousePart.
+   * @param stock The current stock for the InHousePart.
+   * @param min The minimum stock level for the InHousePart.
+   * @param max The maximum stock level for the InHousePart.
+   * @param machineID The ID of the machine that produced this InHousepart.
+   */
+  public InHousePart(int id, String name, double price, int stock, int min, int max,
+      int machineID) {
+    super(id, name, price, stock, min, max);
+    this.machineID = machineID;
+  }
+
+  /**
+   * Returns the ID of the machine that produced this part.
+   * @return The int value of the ID for machine that produced this part
+   */
+  public int getMachineID() {
+    return machineID;
+  }
+
+  /**
+   * Sets the machine ID of the machine that produced this part.
+   * @param machineID New int value to set for this machineID.
+   */
+  public void setMachineID(int machineID) {
+    this.machineID = machineID;
+  }
 }
--- src/Model/OutsourcedPart.java
+++ src/Model/OutsourcedPart.java
@@ -11,9 +11,37 @@ public class OutsourcedPart extends Part{
   /**The company name from which the part was purchased.*/
   private String companyName;
 
+  /**
+   * Constructs a new OutSourcedPart using the specified parameters.
+   *
+   * @param id The ID number for the OutSourcedPart.
+   * @param name The name for the OutSourcedPart.
+   * @param price The price for the OutSourcedPart.
+   * @param stock The current stock for the OutSourcedPart.
+   * @param min The minimum stock level for the OutSourcedPart.
+   * @param max The maximum stock level for the OutSourcedPart.
+   * @param companyName The name of the company supplying the OutSourcedPart.
+   */
   public OutsourcedPart(int id, String name, double price, int stock, int min, int max,
       String companyName) {
     super(id, name, price, stock, min, max);
     this.companyName = companyName;
   }
+
+  /**
+   * Sets the value of company name to the given String.
+   * @param companyName The string value to be set for this company name.
+   */
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  /**
+   * Returns the string value of company name.
+   * @return The String value of companyName.
+   */
+  public String getCompanyName() {
+    return this.companyName;
+  }
+
 }
