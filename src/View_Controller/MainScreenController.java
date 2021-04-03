1ee968a00e5b60fac1305b0c51fa1ca8ad66ebb3
Sakae Watanabe <meltdownlv@gmail.com>
2021-04-03 11:14:02 -0700

    Cleaned up part 1 requirements and fx:id assignments

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -5,10 +5,21 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
 /**
+ * The MainScreenController class controls the logic for the Main Screen
+ * of the inventory application.
+ *
  * @author Sakae Watanabe
  */
 public class MainScreenController implements Initializable {
 
+  /**
+   * The Initialize method is called when the related .fxml layout is loaded
+   * by the FXMLLoader.
+   *
+   * TODO Add descriptions of the logic added - review if this comment is necessary.
+   * @param location
+   * @param resources
+   */
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
